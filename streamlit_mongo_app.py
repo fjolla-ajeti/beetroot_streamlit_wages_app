4195eb7f3c6f3e956513a3f8f8098b7ad21d929d
fjolla-ajeti <fjollaajeti9@gmail.com>
2023-12-17 19:37:12 +0100

    Update streamlit_mongo_app.py

---
+++ streamlit_mongo_app.py
@@ -0,0 +1,33 @@
+# streamlit_app.py
+
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    try:
+        client = pymongo.MongoClient(**st.secrets['mongo'])
+        return client
+    except pymongo.errors.ServerSelectionTimeoutError as e:
+        st.error(f"Failed to connect to MongoDB: {e}")
+        raise e
+
+client = init_connection()
+
+st.title('Employee Wages App')
+
+st.subheader('Register Employees Manually:')
+employee_name = st.text_input('Name:')
+employee_surname = st.text_input('Sruname:')
+employee_address = st.text_input('Address:')
+
+if st.button('Register Employee'):
+    db = client.mydb
+    employee_collection = db.employees
+    employee_data = {'name':employee_name,
+                     'surname':employee_surname,
+                     'address':employee_address}  
+    employee_collection.insert_one(employee_data)
+    st.success('Employee added!')
